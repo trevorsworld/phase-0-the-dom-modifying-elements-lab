070f8edf4cb6fe6878c29a01ed41f252c1e527a7
trevors banu <trevorskarithi@gmail.com>
2024-03-28 17:28:30 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Your Name is the champion";
+document.body.appendChild(newHeader);
